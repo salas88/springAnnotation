67bdc985e8016a619e1a02bc40947416e3e8bc71
Vladislav Hasporian <vladyslav.cartel@gmail.com>
2020-01-05 16:19:37 +0200

    field injection

--- src/com/vladislav/spring/TennisCoach.java
+++ src/com/vladislav/spring/TennisCoach.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class TennisCoach implements Coach {
 	
+	@Autowired
 	private FortuneService fortuneService;
 	
 	/*
@@ -20,11 +21,13 @@ public class TennisCoach implements Coach {
 
 	}
 	
+	/*
 	// define a setter method
 	@Autowired
 	public void setFortuneService(FortuneService fortuneService) {
 		this.fortuneService = fortuneService;
 	}
+	*/
 
 	@Override
 	public String getDailyWorkout() {
